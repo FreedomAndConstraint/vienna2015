0760e72424afa4c4217b995948a0db38331ce4dd
vveitas <vveitas@gmail.com>
2015-05-12 17:43:49 +0200

    first draft made public

---
+++ js/relativepositioning.js
@@ -0,0 +1,55 @@
+function relativePositioning() {
+    var elements = document.querySelectorAll(".step");
+
+    for (var i = 0; i < elements.length; i++) {
+        var x = elements[i].dataset.x;
+        var y = elements[i].dataset.y;
+        var z = elements[i].dataset.z;
+        if (elements[i].dataset.x === undefined) {
+            x = "0";
+        }
+        if (x.substring(0, 2) === '+=') {
+            var lastStep = elements[i - 1].dataset.x;
+            var position = parseInt(lastStep) + parseInt(x.substring(2,x.length));
+            elements[i].dataset.x = position;
+        } else if (x.substring(0, 2) === '-=') {
+            var lastStep = elements[i - 1].dataset.x;
+            var position = parseInt(lastStep) - parseInt(x.substring(2,x.length));
+            elements[i].dataset.x = position;
+        } else {
+            elements[i].dataset.x = x;
+        }
+
+        if (elements[i].dataset.y === undefined) {
+            y = "0";
+        }
+
+        if (y.substring(0, 2) === '+=') {
+            var lastStep = elements[i - 1].dataset.y;
+            var position = parseInt(lastStep) + parseInt(y.substring(2,y.length));
+            elements[i].dataset.y = position;
+        } else if (y.substring(0, 2) === '-=') {
+            var lastStep = elements[i - 1].dataset.y;
+            var position = parseInt(lastStep) - parseInt(y.substring(2,y.length));
+            elements[i].dataset.y = position;
+        } else {
+            elements[i].dataset.y = y;
+        }
+
+        if (elements[i].dataset.z === undefined) {
+            z = "0";
+        }
+
+        if (z.substring(0, 2) === '+=') {
+            var lastStep = elements[i - 1].dataset.z;
+            var position = parseInt(lastStep) + parseInt(z.substring(2,z.length));
+            elements[i].dataset.z = position;
+        } else if (z.substring(0, 2) === '-=') {
+            var lastStep = elements[i - 1].dataset.z;
+            var position = parseInt(lastStep) - parseInt(z.substring(2,z.length));
+            elements[i].dataset.z = position;
+        } else {
+            elements[i].dataset.z = z;
+        }
+    }
+}
